9a08b06d9a2bf7255f3bd658e351ba3cb4cc9c95
Anshul <111146365+anshFE97@users.noreply.github.com>
2023-09-09 13:23:24 +0530

    Add files via upload

---
+++ js_cssVariables/index.js
@@ -0,0 +1,14 @@
+// const spacing = document.querySelector("#spacing")
+// const blur = document.querySelector("#blur")
+// const base = document.querySelector("#base")
+
+const inputs = document.querySelectorAll('.controls input')
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
